b719b33c3741bb96570c9f7cc74eb4ae340cea45
Gauthier Provost <gauthier@kobol.io>
2020-05-27 10:59:58 +0800

    Bump version and add .gitignore

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name='lightgallery',
-    version='0.1.1',
+    version='0.1.2',
     author='Gauthier Provost',
     author_email='gauthier@kobol.io',
     description='Markdown extension to wrap images in lightbox/lightgallery',
     py_modules=['lightgallery'],
-    install_requires = ['markdown>=2.5'],
+    install_requires = ['markdown>=3.0'],
     classifiers=['Topic :: Text Processing :: Markup :: HTML'],
     license='MIT License',
 )
-
